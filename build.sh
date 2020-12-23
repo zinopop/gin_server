b915b26828a8855aa6abf8923c8c9de88ac194ce
zinopop <yuzheng1117@163.com>
2020-12-23 11:16:27 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+sudo docker stop dingTalk && docker rm dingTalk
+sudo docker build -t server .
+
 cd /home/admin/gin_server_v1
 sudo git pull
-sudo docker build -t server:v2 .
-sudo docker stop dingTalk
-sudo docker run -d -p 9001:9001 --name dingTalk server:v2
+
+sudo docker run -d -p 9001:9001 --name dingTalk server
