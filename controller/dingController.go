accad4294322d9a683d4cf5cc858f1a15033d316
zinopop <yuzheng1117@163.com>
2020-12-23 11:24:13 +0800

    测试

--- controller/dingController.go
+++ controller/dingController.go
@@ -15,7 +15,7 @@ type SendMsg struct {
 func DingSend(c *gin.Context) {
 	var SendMsg SendMsg
 	if err := c.ShouldBind(&SendMsg); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "asdasdasd"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	msg, err := helper.SendDingMsg(string(helper.ContextPush(SendMsg.Text)))
