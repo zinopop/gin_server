2863b85074212281f0cb9ca1a0d7a7fd6f08a12d
zinopop <yuzheng1117@163.com>
2020-12-23 14:42:57 +0800

    测试

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-sudo curl http://54.215.230.116:9001/sendMsg?text=开始构建服务...
+#sudo curl http://54.215.230.116:9001/sendMsg?text=开始构建服务...
 sudo docker stop dingTalk
 sudo docker rm dingTalk
+sudo docker rmi server:latest
 
 cd /home/admin/gin_server_v1
-sudo curl http://54.215.230.116:9001/sendMsg?text=最新代码拉取...
+#sudo curl http://54.215.230.116:9001/sendMsg?text=最新代码拉取...
 sudo git pull
 sudo docker build -t server .
 
