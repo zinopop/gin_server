5e6f7a08ccb64e9a1f42baae930991d7e79c8c10
zinopop <yuzheng1117@163.com>
2020-12-23 18:03:18 +0800

    测试

--- build_ding.sh
+++ build_ding.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #sudo curl http://54.215.230.116:9001/sendMsg?text=开始构建服务...
-sudo docker stop dingTalk
-sudo docker rm dingTalk
-sudo docker rmi dingTalk:latest
+sudo docker stop dingtalk
+sudo docker rm dingtalk
+sudo docker rmi dingtalk:latest
 
 cd /home/admin/gin_server_v1
 #sudo curl http://54.215.230.116:9001/sendMsg?text=最新代码拉取...
 sudo git pull
-sudo docker build -t dingTalk .
+sudo docker build -t dingtalk .
 
-sudo docker run -d -p 9001:9001 --name dingTalk dingTalk
+sudo docker run -d -p 9001:9001 --name dingtalk dingtalk
