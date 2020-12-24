11699132a13e5d362b21041f30ebf95099981253
zinopop <yuzheng1117@163.com>
2020-12-24 10:22:36 +0800

    测试

--- controller/webhookController.go
+++ controller/webhookController.go
@@ -1,23 +1,47 @@
 package controller
 
 import (
+	"bytes"
 	"github.com/gin-gonic/gin"
+	"io"
+	"os"
 	"os/exec"
+	"syscall"
 )
+
+type Cmd struct {
+	Path  string    //运行命令的路径，绝对路径或者相对路径
+	Args  []string  // 命令参数
+	Env   []string  //进程环境，如果环境为空，则使用当前进程的环境
+	Dir   string    //指定command的工作目录，如果dir为空，则comman在调用进程所在当前目录中运行
+	Stdin io.Reader //标准输入，如果stdin是nil的话，进程从null device中读取（os.DevNull），stdin也可以时一个文件，否则的话则在运行过程中再开一个goroutine去
+
+	Stdout       io.Writer //标准输出
+	Stderr       io.Writer //错误输出，如果这两个（Stdout和Stderr）为空的话，则command运行时将响应的文件描述符连接到os.DevNull
+	ExtraFiles   []*os.File
+	SysProcAttr  *syscall.SysProcAttr
+	Process      *os.Process      //Process是底层进程，只启动一次
+	ProcessState *os.ProcessState //ProcessState包含一个退出进程的信息，当进程调用Wait或者Run时便会产生该信息．
+}
 
 func Rebuild(c *gin.Context) {
 	command := "/home/admin/gin_server_v1/build.sh ."
-	cmd := exec.Command("/bin/bash", "-c", command)
-	err := cmd.Start()
-	if err != nil {
-		c.JSON(500, gin.H{
-			"data":    "",
-			"message": "命令行错误",
-		})
-		return
-	}
+	cmd := CmdBash(command)
 	c.JSON(200, gin.H{
 		"data":    "",
-		"message": command,
+		"message": cmd,
 	})
+}
+
+func CmdBash(commandName string) *exec.Cmd {
+	cmd := exec.Command("/bin/bash", "-c", commandName)
+
+	go func() {
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		cmd.Stderr = os.Stderr
+		_ = cmd.Run()
+	}()
+
+	return cmd
 }
