c6d72c85bab79432aad7535b2b1a3feb93d10ed8
zinopop <yuzheng1117@163.com>
2020-12-24 13:42:00 +0800

    测试

--- routes/route.go
+++ routes/route.go
@@ -7,7 +7,7 @@ import (
 
 func init() {
 	//钉钉消息推送(普通消息)
-	middleware.Server.POST("/sendMsg", controller.DingSend)
+	middleware.Server.Any("/sendMsg", controller.DingSend)
 	//测试数据库
 	middleware.Server.POST("/dbTest", controller.DbText)
 	//webhook自动构建
