71d45fc787d134d8b8593c0a711c7476c31f75dd
zinopop <yuzheng1117@163.com>
2020-12-24 13:45:39 +0800

    测试

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ sudo docker rm gin_server
 sudo docker rmi gin_server:latest
 
 cd /home/admin/gin_server_v1
-#sudo curl http://54.215.230.116:9001/sendMsg?text=最新代码拉取...
+sudo curl http://54.215.230.116:9001/sendMsg?text=最新代码拉取...
 sudo git pull
 sudo docker build -t gin_server .
 
 sudo docker run -d -p 9002:9002 --name gin_server gin_server
+sudo curl http://54.215.230.116:9001/sendMsg?text=构建成功并运行,服务端口开启:9002...
