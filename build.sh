4a3daf02c50f4a1b11e756517880f6117b515b00
zinopop <yuzheng1117@163.com>
2020-12-30 08:50:02 +0800

    build脚本更新

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ sudo curl http://54.215.230.116:9001/sendMsg?text=开始构建服务...
 sudo docker stop gin_server
 sudo docker rm gin_server
 sudo docker rmi gin_server:latest
-
 cd /home/admin/gin_server_v1
-sudo curl http://54.215.230.116:9001/sendMsg?text=最新代码拉取...
-sudo git pull
-sudo docker build -t gin_server .
 
+sudo curl http://54.215.230.116:9001/sendMsg?text=代码热更新中...
+sudo git pull
+
+sudo curl http://54.215.230.116:9001/sendMsg?text=开始构建并运行中...
+sudo docker build -t gin_server .
 sudo docker run -d -p 9002:9002 --name gin_server gin_server
+sudo docker rmi $(sudo docker images -f "dangling=true" -q)
 sudo curl http://54.215.230.116:9001/sendMsg?text=构建成功并运行,服务端口开启:9002...
